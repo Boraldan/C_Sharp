551d347ddca0aeb6cf40d4ba24a877221dcd8b91
Alex Borodulin <boraldan@gmail.com>
2022-10-12 11:57:04 +0300

    DZ 4 task 1

---
+++ DZ4/1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter degree of number: ");
+int degreeNumber = int.Parse(Console.ReadLine()!);
+ 
+double Degree(int a, int b) {
+    double result = Math.Pow(a,b);
+    return result;
+}
+
+Console. WriteLine($"Degree {degreeNumber} of number {number} = {Degree(number, degreeNumber)}");
