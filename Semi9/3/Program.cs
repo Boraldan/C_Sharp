3208621bc56602bd82591aaabed822d7ae9aacba
Alex Borodulin <boraldan@gmail.com>
2022-11-12 18:48:11 +0300

    next

--- Semi9/3/Program.cs
+++ Semi9/3/Program.cs
@@ -1,21 +1,9 @@
 ﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
 // 453 -> 12
 // 45 -> 9
+ 
+ 
 
-// не верное решение, но рабочее 
-// Console.Clear();
-// Console.Write("Введите количество строк массива: ");
-// int num = int.Parse(Console.ReadLine());
-
-// Console.Write(Sum(num));
-
-// int Sum (int n) {
-
-// if (n < 1) return 0;
-
-// return n + Sum (n-1);
-
-// }
 
 Console.Clear();
 Console.Write("Введите количество строк массива: ");
@@ -23,10 +11,8 @@ int num = int.Parse(Console.ReadLine());
 
 Console.Write(Sum(num));
 
-int Sum (int n) {
-
-if (n < 1) return n;
-
-return n%10 + Sum (n/10);
-
+int Sum (int n) 
+{ 
+    if (n < 1) return n;
+    return n%10 + Sum (n/10);
 }
